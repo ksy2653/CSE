5924826d06a8f79f32d4c7e66913c2e350a971bf
ksy2653 <kim2653seok@gmail.com>
2021-04-06 03:46:17 +0900

    assignment2.1

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function FirstPost() {
   return (
     <>
-      <h1>Covid-19 Out!</h1>
+      <h1>Covid-19 Out!!</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
